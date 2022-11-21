99ecb55420cb3bc4b8b3feadf82c8ae8f2f0e350
HackerHugge <82474677+HackerHugge@users.noreply.github.com>
2022-11-21 16:23:12 +0100

    Försök1

--- src/views/DispatcherView.vue
+++ src/views/DispatcherView.vue
@@ -1,11 +1,14 @@
 <template>
     <div id="orders">
       <div id="orderList">
+
         <div v-for="(order,key) in orders" v-bind:key="'order'+key">
           #{{key}}: Order: {{order.orderedItems}}
           <dd>Contact info: <br>Full name: {{order.details.firstname}} <br>E-mail: {{order.details.email}}<br>Gender: {{order.details.gender}}<br>Payment option: {{order.details.paymentInfo}}<hr></dd>
         </div>
-        <button v-on:click="clearQueue">Clear Queue</button>
+
+
+          <button v-on:click="clearQueue">Clear Queue</button>
       </div>
       <div id="dots" v-bind:style="{ background: 'url(' + require('../../public/img/observableUniverse.jpg')+ ')' }">
           <div v-for="(order, key) in orders" v-bind:style="{ left: order.details.x + 'px', top: order.details.y + 'px'}" v-bind:key="'dots' + key">
@@ -55,6 +58,7 @@
     height: 1078px;
     cursor: crosshair;
   }
-  hr {width: 100%; height: 0px; margin-left: auto; margin-right: auto;}
-  </style>
+  hr {width: 100%; height: 1px; margin-left: auto; margin-right: auto;}
+
+</style>
 
